77376bbeedf8e241f94613dcdcb8da2e71bc8a77
Özgür Baserdem <gurr__@hotmail.com>
2021-09-29 14:23:05 +0200

    Add post new item route

--- routes/api.js
+++ routes/api.js
@@ -1,49 +1,46 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 let items = [
-    {
-        id: 1,
-        title: "Hej",
-        body: "Jag är item 1"
-    },
-    {
-        id: 2,
-        title: "Hej 2",
-        body: "Jag är item 2"
-    },
-    {
-        id: 3,
-        title: "Hej 3",
-        body: "Jag är item 3"
-    }
-]
+  {
+    id: 1,
+    title: "Hej",
+    body: "Jag är item 1"
+  },
+  {
+    id: 2,
+    title: "Hej 2",
+    body: "Jag är item 2"
+  },
+  {
+    id: 3,
+    title: "Hej 3",
+    body: "Jag är item 3"
+  }
+];
 
 /* GET ALL ITEMS */
-router.get('/items', function (req, res, next) {
-    res.json(items);
+router.get("/items", function(req, res, next) {
+  res.json(items);
 });
 
 /* GET ITEM BY ID */
-router.get('/item/:id', function (req, res, next) {
-    const itemId = req.params.id;
-    const item = items.find(item => item.id == itemId);
-    res.json(item);
+router.get("/item/:id", function(req, res, next) {
+  const itemId = req.params.id;
+  const item = items.find(item => item.id == itemId);
+  res.json(item);
 });
 
 /* POST NEW ITEM */
-router.post('/items', function (req, res, next) {
-    const newItem = req.body;
-    items.forEach((item) => {
-        if (newItem.id <= item.id) {
-            newItem.id = item.id;
-        }
-    });
-    newItem.id++;
-    console.log(newItem);
+router.post("/items", function(req, res, next) {
+  const newItem = {
+    title: req.body.title,
+    body: req.body.body
+  };
+
+  res.json(newItem);
+
+//   console.log(newItem);
 });
-
-
-
 
 module.exports = router;
