69fb4fc3a4ca8aa4990bbbfa13c320fc1c6da9ce
Özgür Baserdem <gurr__@hotmail.com>
2021-10-03 14:21:02 +0200

    Add delete single todo and useHistory

--- client/src/fetches/TodoFetches.js
+++ client/src/fetches/TodoFetches.js
@@ -6,12 +6,16 @@ const url = axios.create({
 
 export const insertTodo = payload => url.post(`/todos`, payload);
 export const getAllTodos = () => url.get("/todos");
-export const getSingleTodo = payload => url.get("/todos/:id", { params: { id: payload } });
+export const getSingleTodo = payload =>
+  url.get("/todos/:id", { params: { id: payload } });
+export const deleteTodoById = payload =>
+  url.delete("/todos/:id", { params: { id: payload } });
 
 const todoFetches = {
   insertTodo,
   getAllTodos,
-  getSingleTodo
+  getSingleTodo,
+  deleteTodoById
 };
 
 export default todoFetches;
--- client/src/pages/TodoItem.js
+++ client/src/pages/TodoItem.js
@@ -1,20 +1,27 @@
 import React, { useEffect, useState } from "react";
 import todoFetches from "../fetches/TodoFetches";
+import { useHistory } from "react-router-dom";
 
 export default function TodoItem(props) {
   const payload = props.match.params.id;
+  const history = useHistory();
 
   const [data, setData] = useState({});
 
   useEffect(() => {
-    todoFetches.getSingleTodo(payload)
-      .then(res => setData(res.data));
-  }, [])
+    todoFetches.getSingleTodo(payload).then(res => setData(res.data));
+  }, []);
+
+  function deleteTodo() {
+    todoFetches.deleteTodoById(payload).then(() => history.push("/todos"));
+    console.log(payload);
+  }
 
   return (
     <div>
       <h1>{data.title}</h1>
       <p>{data.body}</p>
+      <button onClick={deleteTodo}>Delete Todo</button>
     </div>
-  )
+  );
 }
