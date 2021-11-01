2deb5b6229c59603a807394595259ebe0dc30c5a
Robin Hedlund <robin.live@live.se>
2021-11-02 00:22:18 +0100

    added landingpage

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,29 @@ import "./App.css";
 import React from "react";
 import Todo from "./pages/Todo";
 import TodoItem from "./pages/TodoItem";
+import LandingPage from "./pages/LandingPage";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route path="/todos/:id" component={TodoItem} />
-        <Route path="/">
-          <Todo />
-        </Route>
-      </Switch>
+    <div class='body'>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Switch>
+          <Route path="/todos/:id" component={TodoItem} />
+          <Route path="/todos" component={Todo} />
+          <Route path="/" component={LandingPage} />
+        </Switch>
+      </ThemeProvider>
     </div>
   );
 }
