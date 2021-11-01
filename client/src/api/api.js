6e49d21ed3e875affd85578e035a1f6f5274ac5d
Robin Hedlund <robin.live@live.se>
2021-11-01 20:02:51 +0100

    change name to api and added endpoints

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -4,19 +4,25 @@ const url = axios.create({
   baseURL: "http://localhost:5000/api"
 });
 
+//TODO endpoints
 export const insertTodo = payload => url.post(`/todos`, payload);
 export const getAllTodos = () => url.get("/todos");
 export const getSingleTodo = id => url.get(`/todos/${id}`);
 export const deleteTodoById = id => url.delete(`/todos/${id}`);
 export const editTodoById = (payload, id) => url.post(`/todos/${id}`, payload);
 
+//USER endpoints
+export const loginUser = payload => url.post("/users/login", payload);
+export const addNewUser = payload => url.post("/users", payload);
 
-const todoFetches = {
+const api = {
   insertTodo,
   getAllTodos,
   getSingleTodo,
   deleteTodoById,
-  editTodoById
+  editTodoById,
+  addNewUser,
+  loginUser
 };
 
-export default todoFetches;
+export default api;
