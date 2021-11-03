11b78dc6f20813a15dc1af4ae247e37cae3e358b
Özgür Baserdem <gurr__@hotmail.com>
2021-11-03 20:47:57 +0100

    Added auth middleware

---
+++ server/middlewares/authUser.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+function authUser(req, res, next) {
+    try {
+        const token = req.header("token");
+        if (!token) return res.status(401).json({ errorMessage: 'Unauthorized' });
+
+        const verified = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+
+        req.user = verified;
+
+        next();
+    } catch (err) {
+        console.error(err);
+        return res.status(401).json({ errorMessage: 'Unauthorized' });
+    }
+}
+
+module.exports = authUser;
