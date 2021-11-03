76a297082c762693d367ef7edbd51085b096ffc8
Özgür Baserdem <gurr__@hotmail.com>
2021-11-03 20:47:49 +0100

    Refactor code from todo router to todo controller

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,15 +1,53 @@
-// var Todo = require("../models/Todo");
+var Todo = require("../models/Todo");
+var checkUser = require("../utils/checkUser");
 
-// createTodo = (req, res) => {
-//   const body = req.body;
+exports.createTodo = (req, res) => {
+    const userToken = req.headers.token;
+    const userId = checkUser(userToken);
 
-//   console.log("Jag är todo controller");
+    const newTodo = {
+        title: req.body.title,
+        body: req.body.body,
+        author: userId
+    };
 
-//   //   const todo = new Todo(body);
+    const todo = new Todo(newTodo);
+    todo.save().then(todo => res.json(todo));
+};
 
-//   //   todo.save().then(todo => res.json(todo));
-// };
+exports.getTodoById = (req, res) => {
+    itemId = req.params.id;
 
-// module.exports = {
-//   createTodo
-// };
+    Todo.findOne({ _id: itemId }).exec((err, todo) => {
+        res.json(todo);
+    });
+}
+
+exports.getUserTodos = (req, res) => {
+    const userToken = req.headers.token;
+    const userId = checkUser(userToken);
+
+    console.log(userId);
+    Todo.find({ author: userId }).exec((err, todos) => {
+        res.json(todos);
+    });
+}
+
+exports.updateTodo = (req, res) => {
+    const id = req.params.id;
+    const { title, body } = req.body;
+
+    Todo.findOne({ _id: req.params.id }, (err, todo) => {
+        todo.title = title
+        todo.body = body
+        todo.save()
+    })
+}
+
+exports.deleteTodo = (req, res) => {
+    const itemId = req.params.id;
+
+    Todo.findOneAndDelete({ _id: itemId }).exec((err, todo) => {
+        res.json(todo);
+    });
+}
