8231c662e8ef332f6e897a5701e8c0b6e9806628
Robin Hedlund <robin.live@live.se>
2021-11-09 13:18:31 +0100

    deleted route and controller

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-const { addNewUser, getUser, loginUser } = require('../controllers/users');
+const { addNewUser, loginUser } = require('../controllers/users');
 
-router.get('/', getUser);
 router.post('/login', loginUser);
 router.post('/', addNewUser);
 
