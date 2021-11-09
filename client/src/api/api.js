8dfec59cc93de74b182960f0a4c7a2a8bfc6e3b4
Robin Hedlund <robin.live@live.se>
2021-11-09 14:16:13 +0100

    update baseURL

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from '../utils/tokenHandlers';
 
 const url = axios.create({
-  baseURL: 'https://evening-brook-20330.herokuapp.com/',
+  baseURL: 'https://stormy-basin-76357.herokuapp.com/',
   headers: { token: getToken() },
 });
 
